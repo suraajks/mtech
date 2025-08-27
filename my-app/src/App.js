dec0e8ec6731ca5a2090f60e20aeeb66d5aa5056
suraaj suresh <kssuraaj@gmail.com>
2025-08-27 21:21:32 +0530

    fix(mtech): axios added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,23 @@
+import React , { useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const  getData = async() => {
+    try {
+      const response = await axios.get('/user?ID=12345');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(()=>{
+    getData();
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
